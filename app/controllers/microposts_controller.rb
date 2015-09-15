e1c20d3b1f45d8f4c094e47a1344cd31d035c853
xsmet <xaviersmet@gmail.com>
2015-09-15 20:17:43 +0000

    AJAX post/destroy microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,22 +1,33 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: [:destroy]
+  #respond_to    :html, :js # TODO requires gem responders
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to root_url unless request.xhr?
     else
       @feed_items = []
-      render 'static_pages/home'
+      render 'static_pages/home' unless request.xhr?
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
   
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_url
+    redirect_to request.referrer || root_url unless request.xhr?
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
   private
